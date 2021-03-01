a04173d9b91c3f60cfa38b439fcb3440284e1b17
Malte Kyhos <mkyhos@protonmail.com>
2021-03-01 22:37:28 +0100

    Init get_data() method

--- R/request.R
+++ R/request.R
@@ -88,3 +88,58 @@ get_capability <- function(url) {
 }
 
 Request$set("private", name = "get_capability", value = get_capability)
+
+
+#' Retrieve defined data from service
+#' 
+#' @param layers <character> 
+get_data <- function(layers = NULL) {
+  browser()
+  url_p <- httr::parse_url(self$url)
+  
+  # Fork process, based on service type.
+
+  # Query building:
+  if (private$service == "WMS") {
+    query <- list(
+      version = "1.1.1",
+      request = "GetMap",
+      format = "image/png",
+      styles = "default",
+      layers = layers,
+      SRS = "EPSG:4258"
+    )
+
+  } else if (private$service == "WFS") {
+    type_name <- basename(url)
+    query <- list(
+      version = "2.0.0",
+      request = "GetFeature",
+      srsName = "EPSG:4258",
+      typeNames = type_name
+    )
+  }
+  
+  url_p$query <- query
+
+  f <- tempfile()
+  client <- crul::HttpClient$new(
+    url = httr::build_url(url_p),
+    opts = list(timeout = 60),
+    progress = httr::progress(type = "down")
+    )
+
+  res <- client$get("get", disk = f)
+  res$raise_for_status()
+
+  if (private$service == "WMS") {
+    data <- raster::brick(f)
+  } else if (private$service == "WFS") {
+    data <- sf::read_sf(f)
+  }
+
+  self$data <- data
+
+}
+
+Request$set("public", name = "get_data", value = get_data)
